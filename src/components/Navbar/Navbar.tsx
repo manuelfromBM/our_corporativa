519a6aa13339b47666e114f121436d478de91617
manuelfromBM <ariel.bm.enterprise@gmail.com>
2025-10-06 20:32:43 -0300

    Primer commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React, {useState} from "react";
+import styles from './Navbar.module.css';
+
+const Navbar:React.FC = () => {
+    const [open, setOpen] = useState<boolean>(false);
+
+    return (
+        
+    )
+}
