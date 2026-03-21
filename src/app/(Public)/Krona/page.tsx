77245940a71054e2cb9f521ec2e2b48c717d3c5b
Matias <matiasp.program@gmail.com>
2026-03-21 19:54:53 -0300

    arreglo de errores de our corporativa

--- src/app/(Public)/Krona/page.tsx
+++ src/app/(Public)/Krona/page.tsx
@@ -1,12 +1,12 @@
-// import BeneficiosKrona from "@/components/Krona-components/Beneficios/BeneficiosKrona";
-// import ComoFunciona from "@/components/Krona-components/Como_Funciona/ComoFunciona";
-// import CTAKrona from "@/components/Krona-components/CTA/CtaKrona";
-// import DashboardKronaSection from "@/components/Krona-components/DashboardPreview/DashboardKrona";
-// import FuncionalidadesKrona from "@/components/Krona-components/Funcionalidades/FuncionalidadesKrona";
-// import SectionHero from "@/components/Krona-components/Hero/Hero";
+import BeneficiosKrona from "@/components/Krona-components/Beneficios/BeneficiosKrona";
+import ComoFunciona from "@/components/Krona-components/Como_Funciona/ComoFunciona";
+import CTAKrona from "@/components/Krona-components/CTA/CtaKrona";
+import DashboardKronaSection from "@/components/Krona-components/DashboardPreview/DashboardKrona";
+import FuncionalidadesKrona from "@/components/Krona-components/Funcionalidades/FuncionalidadesKrona";
+import SectionHero from "@/components/Krona-components/Hero/Hero";
 import NavKronaSection from "@/components/Krona-components/NavBarKrona/NavKrona";
-// import ProblemasKonaSection from "@/components/Krona-components/Problemas/ProblemasKrona";
-// import PublicoObjetivo from "@/components/Krona-components/PublicoObjetivoKrona/PublicoObjetivo";
+import ProblemasKonaSection from "@/components/Krona-components/Problemas/ProblemasKrona";
+import PublicoObjetivo from "@/components/Krona-components/PublicoObjetivoKrona/PublicoObjetivo";
 import ReseñasKrona from "@/components/Krona-components/Reseñas/ReseñasKrona";
 import "@/Styles/Krona-theme.css";
 
@@ -14,15 +14,15 @@ import "@/Styles/Krona-theme.css";
 export default function Kronapage (){
     return( <div className="Krona-theme">
                 <NavKronaSection />
-                {/*<SectionHero />
+                <SectionHero />
                 <PublicoObjetivo/>
                 <ProblemasKonaSection/>
                 <DashboardKronaSection/>
                 <ComoFunciona/>
                 <BeneficiosKrona/>
-                <FuncionalidadesKrona/> */}
+                <FuncionalidadesKrona/>
                 <ReseñasKrona/>
-                {/* <CTAKrona/> */}
+                <CTAKrona/>
             </div> 
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import SeccionContacto from "@/components/Contacto/Contacto";
 import Servicios from "@/components/services/Services";
 import Inicio from "@/components/Home/Home";
 import WhatsAppButton from "@/components/BtnWasap/BtnWasap";
+import NavbarPrincipal from "@/components/Navbar/Navbar";
 
 
 export default function Home() {
   return (
     <>
+      <NavbarPrincipal/>
       <Inicio />
       <Servicios />
       <WhatsAppButton />
